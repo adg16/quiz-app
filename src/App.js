0434aff803a3e47bd8c0ff2d46f2ff8a14d4514a
Aris Gotis <aris.gotis@gmail.com>
2024-03-17 13:11:52 +0800

    hash router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "#components/Home";
 import QuizList from "#components/QuizList";
 import Quiz from "#components/quiz/Quiz";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/quizzes" element={<QuizList />} />
         <Route path="/quiz" element={<Quiz />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
